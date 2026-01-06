8cfb9a76e2ab338fd04bfb16bf7d5b017a8180cb
PASINDU LAKSHAN <pasindul586@gmail.com>
2026-01-06 22:22:03 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,26 @@ function Footer() {
           <h3>BrotherHood Photography</h3>
           <p>Capturing moments that last forever. Professional photography services for all your special occasions.</p>
           <div className="social-links">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">📘</a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">📷</a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">🐦</a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">💼</a>
+            <a href="https://www.facebook.com/profile.php?id=100087684582662" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="social-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12z" />
+              </svg>
+            </a>
+            <a href="https://instagram.com/botherhoodphotography" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="social-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z" />
+              </svg>
+            </a>
+            <a href="https://twitter.com/botherhoodphoto" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="social-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 5.835L18.244 2.25zM17.06 21.75h-1.89l-5.82-7.852l-.7-1.123l-1.16-1.81l-.7-1.123l-5.82-7.852h1.89l5.82 7.852l.7 1.123l1.16 1.81l.7 1.123l5.82 7.852z" />
+              </svg>
+            </a>
+            <a href="https://linkedin.com/company/botherhood-photography" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" className="social-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
+            </a>
           </div>
         </div>
         
@@ -41,9 +57,9 @@ function Footer() {
         
         <div className="footer-section">
           <h4>Contact Info</h4>
-          <p>📍 123 Photography Street<br />Colombo, Sri Lanka</p>
-          <p>📧 info@botherhoodphoto.com</p>
-          <p>📞 +94 XX XXX XXXX</p>
+          <p>📍 75/A Dikwela<br />Yogiyana, Sri Lanka</p>
+          <p>📧 pramodyaanuth17@gmail.com</p>
+          <p>📞 +94 76 644 0370</p>
           <p>⏰ Mon-Fri: 9AM-6PM<br />Sat: 10AM-4PM</p>
         </div>
       </div>
