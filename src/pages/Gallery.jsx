ee250d876af460a19fe86aabf0e896d5047aa7f3
PASINDU LAKSHAN <pasindul586@gmail.com>
2026-01-07 15:42:11 +0530

    add mew photo

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import weddingPhoto1 from '/src/assets/wedding/477036572_592472673685547_4575481749665268043_n.jpg';
 import weddingPhoto2 from '/src/assets/wedding/477098879_592472433685571_1732294956173565579_n.jpg';
 import weddingPhoto3 from '/src/assets/wedding/477217700_592472713685543_9131144071558424404_n.jpg';
+import weddingPhoto4 from '/src/assets/wedding/476931687_592472347018913_8343765868883073735_n.jpg';
 import './Gallery.css';
 
 function Gallery() {
@@ -11,6 +12,7 @@ function Gallery() {
     { id: 1, category: 'wedding', url: weddingPhoto1, title: 'Beautiful Wedding Ceremony' },
     { id: 2, category: 'wedding', url: weddingPhoto2, title: 'Romantic Wedding Moment' },
     { id: 3, category: 'wedding', url: weddingPhoto3, title: 'Wedding Celebration' },
+    {id:13, category:'wedding',url:weddingPhoto4,title:'Bride and Groom' },
     { id: 4, category: 'portrait', url: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=800', title: 'Portrait Session' },
     { id: 5, category: 'landscape', url: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800', title: 'Mountain View' },
     { id: 6, category: 'event', url: 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?w=800', title: 'Event Coverage' },
